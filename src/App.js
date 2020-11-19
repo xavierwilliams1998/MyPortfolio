7dd86fba1c7b64e47ad3f157560409bfef202813
Apple <apple@Apples-Air-2.attlocal.net>
2020-11-18 19:42:09 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+//Components
+import MyNavBar from "./components/my-navbar/mynavbar.component";
+
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  return(<div>
+    <MyNavBar/>
+    My Portfolio</div>)
+  
+  
 }
 
 export default App;
